e10065530ed5cb17ef76753fe5d8cd23aac99de2
AdroTheWanderer <u201612695@upc.edu.pe>
2025-12-05 02:16:29 -0500

    Edit the front-end code with the back-end URL

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://api.render.com/deploy/srv-d4p89163jp1c73duk5s0?key=2pGcmLwgqMc/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://api.render.com/deploy/srv-d4p89163jp1c73duk5s0?key=2pGcmLwgqMc/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
